375431191b2cabd8b665b83ebd6ae31bf1452f0b
b33 <anaclara.castellain@gmail.com>
2023-06-19 10:51:38 -0300

    arrumar logos

--- role/src/visao/OnboardingWindow.java
+++ role/src/visao/OnboardingWindow.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class OnboardingWindow extends JFrame implements ActionListener {
     public OnboardingWindow() {
@@ -20,30 +21,33 @@ public class OnboardingWindow extends JFrame implements ActionListener {
         JPanel contentPane = new JPanel();
         setContentPane(contentPane);
         contentPane.setLayout(null);
-        
-        JLabel texto = new JLabel("Boas-vindas ao");
-        texto.setBounds(60, 40, 400, 100);
-        texto.setForeground(Color.WHITE);
-        texto.setFont(new Font("Inter", Font.BOLD, 36));
-        contentPane.add(texto);
 
         ImageIcon logoIcon = new ImageIcon("role/src/visao/Logo.png");
         JLabel logo = new JLabel(logoIcon);
-        logo.setBounds(150, 110, 83, 33);
+        logo.setBounds(20, 32, 163, 71);
         contentPane.add(logo);
 
         JButton nextCadastro = new JButton("Prosseguir");
         nextCadastro.setForeground(Color.BLACK);
         nextCadastro.setBackground(Color.WHITE);
         nextCadastro.setFont(new Font("Inter", Font.BOLD, 14));
-        nextCadastro.setBounds(114, 175, 154, 37);
+        nextCadastro.setBounds(109, 196, 154, 37);
         nextCadastro.addActionListener(this);
         contentPane.add(nextCadastro);
 
         ImageIcon fundoIcon = new ImageIcon("role/src/visao/Background.png");
-        JLabel fundo = new JLabel(fundoIcon);
-        fundo.setBounds(0, 0, 400, 400);
-        contentPane.add(fundo);
+        
+        JLabel lblNewLabel = new JLabel("Boas-vindas ao seu novo aplicativo de eventos!");
+        lblNewLabel.setForeground(Color.WHITE);
+        lblNewLabel.setFont(new Font("Inter", Font.BOLD, 15));
+        lblNewLabel.setBounds(10, 114, 408, 37);
+        contentPane.add(lblNewLabel);
+        
+        JLabel lblNewLabel_1 = new JLabel("Clique em prosseguir para ir a nossa tela de cadastro.");
+        lblNewLabel_1.setForeground(Color.LIGHT_GRAY);
+        lblNewLabel_1.setFont(new Font("Inter", Font.BOLD | Font.ITALIC, 13));
+        lblNewLabel_1.setBounds(20, 151, 374, 14);
+        contentPane.add(lblNewLabel_1);
     }
 
     public static void main(String[] args) {
