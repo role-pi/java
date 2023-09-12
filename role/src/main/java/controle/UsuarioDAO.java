eeb88ec8980500789765bfa26d3b30f5f864a162
Ana Clara Castellain <111303609+fr0styb33@users.noreply.github.com>
2023-09-12 10:00:24 -0300

    Arrumar o try catch

--- role/src/main/java/controle/UsuarioDAO.java
+++ role/src/main/java/controle/UsuarioDAO.java
@@ -1,9 +1,74 @@
 package controle;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Evento;
 import modelo.Usuario;
 
-public class UsuarioDAO {
+public class UsuarioDAO implements DAO<Usuario> {
+	  private List<Usuario> usuarios = new ArrayList<Usuario>();
+
+	    @Override
+	    public List<Usuario> list() {
+	    	Conexao c = Conexao.getInstancia();
+
+	    	Connection con = c.conectar();
+
+	    	String query = "SELECT * FROM usuários";
+
+	    	usuarios.clear();
+	    	try {
+				PreparedStatement ps = con.prepareStatement(query);
+
+				ResultSet rs = ps.executeQuery();
+				while (rs.next()) {
+					
+					int email = rs.getInt("email");
+
+					String Nome = rs.getString("nome");
+					Usuario u = new Usuario();
+					u.setEmail(email);
+					u.setNome(nome);
+
+					usuarios.add(u);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			c.fecharConexao();
+			return usuarios;
+		}
+	    	
+	    	public boolean inserir(Usuario u) {
+
+	    		Conexao c = Conexao.getInstacia();
+	    		Connection con = c.conectar();
+
+	    		String query = "INSERT INTO usuarios " + "(email, nome) " + "VALUES (?, ?)";
+
+	    		try {
+	    			PreparedStatement ps = con.prepareStatement(query);
+
+	    			ps.setInt(1, p.getEmail());
+	    			ps.setString(2, p.getNome());
+
+	    			ps.executeUpdate();
+
+	    			c.fecharConexao();
+
+	    			return true;
+	    		} catch (SQLException e) {
+	    			e.printStackTrace();
+	    		}
+	    		return false;
+	    	
+	    }
 	private Usuario usuarioCadastrado;
 	
 	// Singleton
