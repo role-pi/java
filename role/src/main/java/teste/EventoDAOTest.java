7e06300af3a0c348764dbcec1969894566013169
João Gabriel <joogps@gmail.com>
2023-10-03 08:39:24 -0300

    Update EventoDAOTest.java

--- role/src/main/java/teste/EventoDAOTest.java
+++ role/src/main/java/teste/EventoDAOTest.java
@@ -1,12 +1,21 @@
 package teste;
 
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
+
+import controle.EventoDAO;
+import modelo.Evento;
 
 public class EventoDAOTest {
 
 	@Test
 	public void testMetodoInserirEvento() {
+		Evento evento = new Evento();
+		evento.setNome("Evento");
 		
+		int id = EventoDAO.getInstance().insert(evento);
+		assertNotEquals(id, 0);
 	}
 	
 }
