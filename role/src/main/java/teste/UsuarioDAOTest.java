1e358d321ef6bf12380f8bbb2373025468990ebd
João Gabriel <joogps@gmail.com>
2023-10-17 08:21:05 -0300

    Corrigir testes

--- role/src/main/java/teste/UsuarioDAOTest.java
+++ role/src/main/java/teste/UsuarioDAOTest.java
@@ -14,16 +14,19 @@ public class UsuarioDAOTest {
 	private UsuarioDAO usuarioDao; 
     private Usuario usuario;
     
+    public UsuarioDAOTest() {
+    	this.usuarioDao = UsuarioDAO.getInstance();
+    	this.usuario = new Usuario(0, "Nome", "Email");
+    }
+    
 	@Test
 	public void testInsertUsuario() {
-		
 		int insertId = usuarioDao.insert(usuario);
 		assertTrue(insertId > 0);
 	}
 	
 	@Test
-	private void testSelectUsuario() {
-		
+	public void testSelectUsuario() {
 		int insertId = usuarioDao.insert(usuario);
         Usuario usuarioInsert = usuarioDao.list().stream()
                 .filter(u -> u.getId() == insertId)
@@ -33,24 +36,22 @@ public class UsuarioDAOTest {
     }
 	
 	@Test
-	private void testUpdateUsuario() {
-		
+	public void testUpdateUsuario() {
 		int insertId = usuarioDao.insert(usuario);
-		 usuario.setId(insertId);
-	        usuario.setNome("Novo nome teste");
-	        usuario.setEmail("novotest@email.com");
-	        assertTrue(usuarioDao.update(usuario));
-	        Usuario usuarioUpdate = usuarioDao.list().stream()
-	                .filter(u -> u.getId() == insertId)
-	                .findFirst()
-	                .orElse(null);
-	        assertEquals("Novo nome teste", usuarioUpdate.getNome());
-	        assertEquals("novotest@email.com", usuarioUpdate.getEmail());
+		usuario.setId(insertId);
+	    usuario.setNome("Novo nome teste");
+	    usuario.setEmail("novotest@email.com");
+	    assertTrue(usuarioDao.update(usuario));
+	    Usuario usuarioUpdate = usuarioDao.list().stream()
+	            .filter(u -> u.getId() == insertId)
+	            .findFirst()
+	            .orElse(null);
+	    assertEquals("Novo nome teste", usuarioUpdate.getNome());
+	    assertEquals("novotest@email.com", usuarioUpdate.getEmail());
 	}
 	
 	@Test
-	private void testDeleteUsuario() {
-		
+	public void testDeleteUsuario() {
 		int insertId = usuarioDao.insert(usuario);
 		usuario.setId(insertId);
         assertTrue(usuarioDao.delete(usuario));
